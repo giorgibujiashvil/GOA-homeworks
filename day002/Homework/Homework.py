443ac7ffbdeb77275dd35825d80654a39fad79f2
giorgibujiashvil <giorgibujiashvili51@gmail.com>
2024-06-25 21:49:48 +0400

    Second-commit

---
+++ day002/Homework/Homework.py
@@ -0,0 +1,5 @@
+gavicani1="nika chunashvili"
+davumegobrdi1="nika dodashvili"
+gavicani2="nika dodashvili"
+davumegobrdi2="nika chunasvili"
+print(gavicani1+" "+davumegobrdi1+" "+gavicani2+" "+davumegobrdi2)
