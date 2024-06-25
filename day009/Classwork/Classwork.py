d670787a208a507a28e2f0ddeeafa9c10f4e74c3
giorgibujiashvil <giorgibujiashvili51@gmail.com>
2024-06-25 21:52:22 +0400

    Ninth-commit

---
+++ day009/Classwork/Classwork.py
@@ -0,0 +1,37 @@
+#task 1
+i = 1 
+
+while i <= 100:
+    print(i)
+    i = i + 1
+
+#taks 2
+i = 100
+
+while i <= 0:
+    print(i)
+    i = i - 1
+
+
+#task 3 
+
+i = 0 
+sum = 0
+
+while i <= 10:
+    print(i)
+    sum = sum + 1
+    i = i + 1
+
+print(sum)
+
+
+#task 4
+    
+i = 1
+
+while i < 10:
+    print(i)
+    i = i + 2
+
+
