d34da7b80e9022cb2d6cda6c544e55c45bcd5ba5
giorgibujiashvil <giorgibujiashvili51@gmail.com>
2024-06-25 21:51:40 +0400

    Seventh-commit

---
+++ day007/Classwork/Classwork.py
@@ -0,0 +1,25 @@
+name = input("enter your name:")
+surname = input("enter your surname:")
+age =int(input("enter your age:"))
+print(name,surname,age)
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number:  "))
+
+num1 = 10
+num2 = 20 
+num3 = 30
+
+
+print(num1 - num2 - num3)
+print(num1 + num2 + num3)
+print(num1 * num2 * num3)
+print(num1 / num2 / num3)
+print(num1 % num2)
+print(num3 // num2)
+
+print(num1 < num2)
+print(num1 > num3)
+print(num1 == num2)
+
---
+++ day007/Homework/Homework.py
@@ -0,0 +1,44 @@
+#We learned in Day 007
+name = input("enter your name:")
+surname = input("enter your surname:")
+age =int(input("enter your age:"))
+print(name,surname,age)
+
+#Convert a string type to an integer
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number:  "))
+
+num1 = 10
+num2 = 20 
+num3 = 30
+#Mathematical operations
+print(num1 - num2 - num3)
+print(num1 + num2 + num3)
+print(num1 * num2 * num3)
+print(num1 / num2 / num3)
+print(num1 % num2)
+print(num3 // num2)
+
+print(num1 < num2)
+print(num1 > num3)
+print(num1 == num2)
+
+print(5 >= 5)
+print(5 <=5)
+
+#Plus +
+#Minus - 
+#multiplication *
+#division /
+#How many times will it fit?  //
+
+#This way we know what type the variable is: print(type(x))
+print(type(2.5))
+
+#Mechanical transmission
+age =int(input("enter your age:"))
+
+#automatic transmission
+age = 32
+age = (str(32)+"age")
