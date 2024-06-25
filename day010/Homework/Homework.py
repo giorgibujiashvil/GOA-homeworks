1cb343bd39489000a97773edc1ea971579d84685
giorgibujiashvil <giorgibujiashvili51@gmail.com>
2024-06-25 21:53:29 +0400

    Tenth-commit

---
+++ day010/Homework/Homework.py
@@ -0,0 +1,87 @@
+#დათა თეზელაშვილის მიერ მოცემული დავალებები 
+
+#მაგალითი 1
+for i in range(20 + 1):
+    print(i)
+
+#მაგალითი 2
+sum = 0
+
+for num in range(50,100 + 1):
+    print(num)
+    sum = sum + num
+    print(sum)
+
+#მაგალითი 3 step-ები
+for i in range(-10,10 + 1 ,3):
+    print(i)    
+
+
+#ბატონი ლუკა ცხვარაძის მიერ მოცემული დავალება:მოვიფიქროტ 10 მაგალიტი for ციკლისთვის
+
+#მაგალითი 1
+
+for i in range(1,6):
+    print(i)
+
+#მაგალითი 2
+
+sum = 0
+for num in range(10,21):
+    print(num)
+
+#მაგალითი 3
+
+sum = 0 
+for num in range(10,21):
+    print(num)
+    sum = num + sum
+    print(sum)
+
+#მაგალითი 4 step-ები
+
+for i in  range(10 , 31, 5):
+    print(i)
+
+#მაგალითი 5
+
+for num in range(2,5):
+    print(num)
+    print(range(2,5))
+
+
+#მაგალითი 6
+
+for num in range(2,5):
+    print(num)
+print(range(2,5))     #outside of the for loop
+
+
+#მაგალითი 7
+
+print(range(3,5))  #outside of the for loop
+
+for num in range(3,5):
+    print(num)
+
+
+#მაგალითი 8
+
+sum = 0
+
+for num in range(2 , 20 + 1):
+    sum = sum + num
+
+
+#მაგალითი 9
+
+sum = 0 
+for num in range(10):
+    print(num)
+    print("girogi")
+    sum = sum + num
+
+#მაგალითი 10
+
+for i in range(100):
+    print("giorgi")
