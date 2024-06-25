370938db7676e7e178772cec73c0b7a80ac4631b
giorgibujiashvil <giorgibujiashvili51@gmail.com>
2024-06-25 21:51:21 +0400

    Sixth-commit

---
+++ day006/Classwork/Classwork006.py
@@ -0,0 +1,5 @@
+name = input("please enter your name:")
+surname= input("please enter your surname:")
+age = input("please enter your age:")
+
+print("plaese enter your name",name,"please enter your surname",surname,"plase enter your age",age,)
---
+++ day006/Homework/Homework006.py
@@ -0,0 +1,20 @@
+name = input("please enter your name:")
+surname= input("please enter your surname:")
+age = input("please enter your age:")
+
+print("plaese enter your name",name,"please enter your surname",surname,"plase enter your age",age,)
+
+# Prompting the user to enter three numbers
+num1 = (int(10.0))
+num2 = (int(20.0))
+num3 = (int(30.0))
+
+num1 = input("Enter the first number:  ")
+num2 = input("Enter the second number: ")
+num3 = input("Enter the third number:  ")
+result1 = num1 - num2 - num3 
+result2 = num1 + num2 + num3
+result3 = num1 * num2 * num3
+result4 = num1 / num2 / num3
+
+print(str)(num1,num2,num3)
