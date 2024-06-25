323f19c05d8e6cb44cf5e60cdfd3c7feb51fbc07
giorgibujiashvil <giorgibujiashvili51@gmail.com>
2024-06-25 21:50:12 +0400

    Third-commit

---
+++ day003/Homework/Homework.py
@@ -0,0 +1,10 @@
+my_momname="tamari"
+my_momsurame="bejanishvili"
+my_dadname="arsena"
+my_dadsurname="bujiashvili"
+my_grandfathername="giorgi"
+my_grandfathersurname="bujiashvili"
+my_grandmothername="marine"
+my_grandmothersurname="goniashvili"
+
+print(my_momname+" "+my_momsurame+" "+my_dadname+" "+my_dadsurname+" "+my_grandfathername+" "+my_grandfathersurname+" "+my_grandmothername+""+my_grandmothersurname)
