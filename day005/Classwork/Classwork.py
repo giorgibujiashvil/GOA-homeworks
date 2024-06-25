aa6d952e9360418f43b048639a8a5536992b2b41
giorgibujiashvil <giorgibujiashvili51@gmail.com>
2024-06-25 21:50:55 +0400

    Fifth-commit

---
+++ day005/Classwork/Classwork.py
@@ -0,0 +1,16 @@
+#declaring value of first price
+first_price = 1000
+
+#declare a value of price after discount
+discount = 20
+
+#declaring value of price after discount
+price_after_discount1 = first_price - first_price * discount / 100
+
+discount = 20
+
+
+price_after_discount2 = first_price - first_price * discount / 100
+#displaying last price
+print(price_after_discount1)
+print(price_after_discount2)
